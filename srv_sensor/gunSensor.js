581b95d9e1d8c7071b4fb1ce2942252f49436646
archimbaud.paul <archimbaud.paul@gmail.com>
2020-03-02 23:24:46 +0100

    dht

--- srv_sensor/gunSensor.js
+++ srv_sensor/gunSensor.js
@@ -4,26 +4,24 @@ const sensor = require("node-dht-sensor");
 
 var Gun = require('gun');
 app.use(Gun.serve);
-
+var gun = Gun({	file: 'data', web: server });
 
 const server = app.listen(3000, function () {
-
 	gun.get('sensorData').on(function(data, key){
-	  console.log("update:", data);
+		console.log("update:", data);
 	});
 })
-var gun = Gun({	file: 'data', web: server });
 
 
 function readSensor() {
-sensor.read(22, 4, function(err, temperature, humidity) {
-	if (!err) {
-		gun.get('sensorData').put({
-			temperature: temperature,
-			humidity:humidity,
-			date: Date.now()
-		});
-	}
-})
+	sensor.read(22, 4, function(err, temperature, humidity) {
+		if (!err) {
+			gun.get('sensorData').put({
+				temperature: temperature,
+				humidity:humidity,
+				date: Date.now()
+			});
+		}
+	})
 }
 setInterval(readSensor, 60000)
