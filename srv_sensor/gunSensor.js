08d92df9e2edda110ec1becce2d5f50724697506
archimbaud.paul <archimbaud.paul@gmail.com>
2020-03-03 19:05:52 +0100

    log

--- srv_sensor/gunSensor.js
+++ srv_sensor/gunSensor.js
@@ -8,6 +8,7 @@ var gun = Gun({file: 'data', web: http.createServer().listen(8765) });
 
 function readSensorDHC22() {
 	sensor.read(22, 4, function(err, temperature, humidity) {
+		if (err) {console.log(err)}
 		if (!err) {
 			const date = Date.now()
 			gun.get('sensorDataV1').get(date).put({
